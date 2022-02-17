a1564db44271d19a65f3954b97978edd4fa3d5ab
ungnv <ungnv@hblab.vn>
2022-02-17 09:35:07 +0700

    feat: update core laravel

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,16 +1,18 @@
 <?php
-
+// Project: 6909328e1e6d640e24bc2ba0b79f902e9be28484446a50
 namespace App\Exceptions;
 
-use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Throwable;
+use Symfony\Component\HttpFoundation\Response;
 
 class Handler extends ExceptionHandler
 {
     /**
      * A list of the exception types that are not reported.
      *
-     * @var array
+     * @var string[]
      */
     protected $dontReport = [
         //
@@ -19,33 +21,57 @@ class Handler extends ExceptionHandler
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
-     * @var array
+     * @var string[]
      */
     protected $dontFlash = [
+        'current_password',
         'password',
         'password_confirmation',
     ];
 
     /**
-     * Report or log an exception.
+     * Register the exception handling callbacks for the application.
      *
-     * @param  \Exception  $exception
      * @return void
      */
-    public function report(Exception $exception)
+    public function register()
     {
-        parent::report($exception);
-    }
+        $this->renderable(function (Throwable $e) {
+            $throwable = false;
+            $messages = [
+                Response::HTTP_INTERNAL_SERVER_ERROR => trans('exception.server_error'),
+                Response::HTTP_METHOD_NOT_ALLOWED => trans('exception.method_not_allowed'),
+                Response::HTTP_UNAUTHORIZED => trans('exception.unauthorized'),
+                Response::HTTP_FORBIDDEN => trans('exception.forbidden'),
+                Response::HTTP_BAD_REQUEST => trans('exception.bad_request'),
+                Response::HTTP_NOT_FOUND => trans('exception.not_found'),
+            ];
+            if ($e instanceof \ParseError) {
+                $code = Response::HTTP_INTERNAL_SERVER_ERROR;
+                $throwable = true;
+            } elseif ($e instanceof HttpException) {
+                $statusCode = $e->getStatusCode();
+                switch ($statusCode) {
+                    case Response::HTTP_NOT_FOUND:
+                    case Response::HTTP_METHOD_NOT_ALLOWED:
+                    case Response::HTTP_UNAUTHORIZED:
+                    case Response::HTTP_FORBIDDEN:
+                    case Response::HTTP_BAD_REQUEST:
+                        $throwable = true;
+                        $code = $statusCode;
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                //continue
+            }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-        return parent::render($request, $exception);
+            if ($throwable) {
+                return response()->json([
+                    'message' => $messages[$code]
+                ], $code);
+            }
+        });
     }
 }
