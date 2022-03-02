73487cbe92d14ad942addd29e0d0f20166553f07
minhnh <minhnh@hblab.vn>
2022-03-02 16:03:55 +0700

    feat/Middleware-descentralization

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Enums\RoleEnum;
+use App\Enums\UserStatusEnum;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
